37bb5a3a28af07813760185fdf5a754b83e56946
Luca Armendariz <larmendarizl23@fpsanturtzilh.eus>
2025-04-16 11:40:56 +0200

    cfhn

--- PeluProject-main/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'PeluProject',
-  webDir: 'www'
-};
-
-export default config;
